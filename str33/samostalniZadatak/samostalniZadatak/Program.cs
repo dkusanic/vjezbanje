2d7312a3446922391e459f05783e4137b90a8e9c
dkusanic <dkusanic@foi.hr>
2018-12-06 18:35:45 +0100

    rijesen zadatak

--- str33/samostalniZadatak/samostalniZadatak/Program.cs
+++ str33/samostalniZadatak/samostalniZadatak/Program.cs
@@ -10,6 +10,14 @@ namespace samostalniZadatak
     {
         static void Main(string[] args)
         {
+            int M = 30;
+            int rezultat = 0;
+            for(int N=10;N<=M;N++)
+            {
+                rezultat += N;
+            }
+            Console.WriteLine("Zborj svih prirodnih brojeva od 10 do 30 je "+rezultat);
+            Console.ReadLine();
         }
     }
 }
