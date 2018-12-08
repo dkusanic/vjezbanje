c18b420987ac0f117b2cbc137b3406e702650471
dkusanic <dkusanic@foi.hr>
2018-12-08 12:36:37 +0100

    ttt

---
+++ str84/vjclass/vjclass/AutomotiveClasses/VIN.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjclass.AutomotiveClasses
+{
+    class VIN
+    {
+        private int wmi;
+        private int vds;
+        private int vis;
+
+        public VIN(int wmi, int vds, int vis)
+        {
+            this.wmi = wmi;
+            this.vds = vds;
+            this.vis = vis;
+        }
+
+        public string getVin()
+        {
+            return wmi + "" + vds + "" + vis;
+        }
+    }
+}
---
+++ str84/vjclass/vjclass/AutomotiveClasses/Vozilo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjclass.AutomotiveClasses
+{
+    abstract class Vozilo
+    {
+        private List<Dio> montiraniDijelovi;
+        public VIN idAutomobila;
+        public string naziv { get; set; }
+        public int status { get; set; }
+        public int tezina { get; set; }
+        public int visina { get; set; }
+        public int duzina { get; set; }
+        public int meduosovinskiRazmak { get; set; }
+        public String gume { get; set; }
+        public int co2 { get; set; }
+
+        public Vozilo()
+        {
+            montiraniDijelovi = new List<Dio>();
+            idAutomobila = new VIN(10,15,54);
+        }
+
+        public bool addDio(Dio noviDio)
+        {
+            montiraniDijelovi.Add(noviDio);
+            return true;
+        }
+
+        public void updateStatus()
+        {
+            status = status++;
+        }
+
+        public List<Dio> getDijelovi()
+        {
+            return montiraniDijelovi;
+        }
+    }
+}
